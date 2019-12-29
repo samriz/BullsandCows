4a36d2db5ea069c1b4e7e67b827742430680f53c
samriz <samriz@users.noreply.github.com>
2019-12-28 21:31:45 -0600

    Inside of the component that I added for the Terminal actor, I am welcoming the player inside of the terminal.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,10 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("This is Bulls and Cows. Enter a fucking word.\nPress enter to continue...");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
