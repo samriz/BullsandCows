adfc3cbc48441523ef936facf20643861e0b2d7e
samriz <samriz@users.noreply.github.com>
2019-12-30 10:55:00 -0600

    Setup basic game premise.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,15 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+FString HiddenWord = TEXT("date");
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("This is Bulls and Cows. Enter a fucking word.\nPress enter to continue..."));
+    PrintLine(TEXT("This is Bulls and Cows."));
+    PrintLine(TEXT("Enter a 4-letter word and press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    FString HiddenWord = TEXT("isogram");
+    
     ClearScreen();
+    PrintLine("Your guess: ");
     PrintLine(Input);
+    if(Input == HiddenWord) PrintLine("Congrats. You won the game.");
+    else PrintLine("That's wrong but keep trying.");
 }
