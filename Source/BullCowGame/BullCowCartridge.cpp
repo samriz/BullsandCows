371cd596d38c68a1243e62c5ea59b3b8ac5436b3
samriz <samriz@users.noreply.github.com>
2019-12-30 18:57:40 -0600

    Declare HiddenWord in header file and define it in BeginPlay(). Also, improved compile time.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,13 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-FString HiddenWord = TEXT("date");
-
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("This is Bulls and Cows."));
     PrintLine(TEXT("Enter a 4-letter word and press enter to continue..."));
+    HiddenWord = TEXT("date");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
